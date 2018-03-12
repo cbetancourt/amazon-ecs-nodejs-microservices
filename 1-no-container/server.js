0cc59bcefa8060189ba32bc62d5f1f3697964fa0
Claude Betancourt <cbetancourt@claudes-mbp15.lan>
2018-03-12 00:03:58 -0400

    Added API for /api/posts.
    Removed useless int parsing since matches use the equality operator.

--- 1-no-container/server.js
+++ 1-no-container/server.js
@@ -3,47 +3,47 @@ const router = require('koa-router')();
 const db = require('./db.json');
 
 // Log requests
-app.use(function *(next){
-  const start = new Date;
-  yield next;
-  const ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
+app.use(function* (next) {
+	const start = new Date;
+	yield next;
+	const ms = new Date - start;
+	console.log('%s %s - %s', this.method, this.url, `${ms}ms`);
 });
 
-router.get('/api/users', function *(next) {
-  this.body = db.users;
+router.get('/api/users', function* (next) {
+	this.body = db.users;
 });
 
-router.get('/api/users/:userId', function *(next) {
-  const id = parseInt(this.params.userId);
-  this.body = db.users.find((user) => user.id == id);
+router.get('/api/users/:userId', function* (next) {
+	this.body = db.users.find((user) => user.id == this.params.userId);
 });
 
-router.get('/api/threads', function *() {
-  this.body = db.threads;
+router.get('/api/threads', function* () {
+	this.body = db.threads;
 });
 
-router.get('/api/threads/:threadId', function *() {
-  const id = parseInt(this.params.threadId);
-  this.body = db.threads.find((thread) => thread.id == id);
+router.get('/api/threads/:threadId', function* () {
+	this.body = db.threads.find((thread) => thread.id == this.params.threadId);
 });
 
-router.get('/api/posts/in-thread/:threadId', function *() {
-  const id = parseInt(this.params.threadId);
-  this.body = db.posts.filter((post) => post.thread == id);
+router.get('/api/posts', function* () {
+	this.body = db.posts;
 });
 
-router.get('/api/posts/by-user/:userId', function *() {
-  const id = parseInt(this.params.userId);
-  this.body = db.posts.filter((post) => post.user == id);
+router.get('/api/posts/in-thread/:threadId', function* () {
+	this.body = db.posts.filter((post) => post.thread == this.params.threadId);
 });
 
-router.get('/api/', function *() {
-  this.body = "API ready to receive requests";
+router.get('/api/posts/by-user/:userId', function* () {
+	this.body = db.posts.filter((post) => post.user == this.params.userId);
 });
 
-router.get('/', function *() {
-  this.body = "Ready to receive requests";
+router.get('/api/', function* () {
+	this.body = "API ready to receive requests";
+});
+
+router.get('/', function* () {
+	this.body = "Ready to receive requests";
 });
 
 app.use(router.routes());
